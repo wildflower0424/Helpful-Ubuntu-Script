ddf2bd4588461fbe6535448f564461dbdc8d2ab1
wildflower0424 <106394504+wildflower0424@users.noreply.github.com>
2022-06-28 05:26:22 -0700

    Create Getting-Started-With-Luke.sh

---
+++ Getting-Started-With-Luke.sh
@@ -0,0 +1,4 @@
+ #!/bin/bash
+ 
+ echo "Hello, My Name Is Luke, Thank you For Using My Script. This Script will install packages like Git-Core,Nodejs v16.15.1, Yarn and update+Upgrade your Ubuntu Packages
+ sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install git-core
