ecb15f007c13584cd7e50f12136c3263199397a5
wildflower0424 <106394504+wildflower0424@users.noreply.github.com>
2022-07-06 03:28:44 -0700

    Update Getting-Started-With-Luke.sh

--- Getting-Started-With-Luke.sh
+++ Getting-Started-With-Luke.sh
@@ -1,18 +1,15 @@
  #!/bin/bash
  
- echo "Hello, My Name Is Luke, Thank you For Using My Script. This Script will install packages like Curl,Nodejs v16.15.1, Yarn and update+Upgrade your Ubuntu Packages
- pause 5
+ echo "Hello, My Name Is Luke, Thank you For Using My Script. This Script will install packages like Curl,Nodejs v16.15.1, Yarn and update+Upgrade your Ubuntu Packages"
  sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install -y curl
  echo "Installing NodeJS V= 16.15.1"
- pause 5
  curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
  sudo apt-get install -y nodejs
  echo "Installing Yarn"
- pause 5
  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update && sudo apt install -y yarn
 sudo apt-get update && sudo apt-get upgrade -y
-echo "Thank You for using my script we are done installing now. Have a Great Rest Of Your Day."
-echo "Good Bye ----Luke Angel L.A."
+echo "Thank You for using my script we are done installing now. Have a Great Rest Of Your Day"
+echo "Good Bye ----Luke Angel"
 sleep 5
