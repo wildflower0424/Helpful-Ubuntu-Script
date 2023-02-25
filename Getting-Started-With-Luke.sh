a812139637edb8675af658d066c5decee75e0630
wildflower0424 <106394504+wildflower0424@users.noreply.github.com>
2023-02-25 11:59:11 -0700

    Update Getting-Started-With-Luke.sh

--- Getting-Started-With-Luke.sh
+++ Getting-Started-With-Luke.sh
@@ -1,15 +1,20 @@
- #!/bin/bash
+#!/bin/bash
  
  echo "Hello, My Name Is Luke, Thank you For Using My Script. This Script will install packages like Curl,Nodejs v16.15.1, Yarn and update+Upgrade your Ubuntu Packages"
+ sleep 2
  sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install -y curl
  echo "Installing NodeJS V= 16.15.1"
+ sleep 3
  curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
  sudo apt-get install -y nodejs
+ sudo npm install -g npm@latest
  echo "Installing Yarn"
+ sleep 3
  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt update && sudo apt install -y yarn
-sudo apt-get update && sudo apt-get upgrade -y
-echo "Thank You for using my script we are done installing now. Have a Great Rest Of Your Day"
-echo "Good Bye ----Luke Angel"
-sleep 5
+ echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+ sudo apt update && sudo apt install -y yarn=1.19.1-1
+ sudo apt-get update && sudo apt-get upgrade -y
+ sleep 2
+ echo "Thank You for using my script we are done installing now. Have a Great Rest Of Your Day"
+ echo "Good Bye ----Luke Angel"
+ sleep 5
