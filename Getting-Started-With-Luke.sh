a95976804079d93c60ebeb625777ff071b32dde0
wildflower0424 <106394504+wildflower0424@users.noreply.github.com>
2023-03-25 09:44:41 -0700

    Update Getting-Started-With-Luke.sh

--- Getting-Started-With-Luke.sh
+++ Getting-Started-With-Luke.sh
@@ -2,11 +2,29 @@
  
  echo "Hello, My Name Is Luke, Thank you For Using My Script. This Script will install packages like Curl,Nodejs v16.15.1, Yarn and update+Upgrade your Ubuntu Packages"
  sleep 2
+ 
+ 
+ while true; do
+
+read -p "do you want to continue? (y/n) " yn
+
+case $yn in 
+	[yY] ) echo ok, we will proceed;
+		break;;
+	[nN] ) echo exiting...;
+		exit;;
+	* ) echo invalid response;;
+esac
+
+done 
+
+
+
  sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install -y curl
  echo "Installing NodeJS V= 16.15.1"
  sleep 3
  curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
- sudo apt-get install -y nodejs=16.19.1
+ sudo apt-get install -y nodejs=16.15.1
  sudo npm install -g npm@latest
  echo "Installing Yarn"
  sleep 3
